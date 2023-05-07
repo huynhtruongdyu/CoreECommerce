f797c152428cfd29dcac05c772067cb8944b291c
Duy Huỳnh <huynhtruongdyu@gmail.com>
2023-05-07 21:50:01 +0700

    add Product with image

---
+++ src/CEC.WebMVC/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CEC.WebMVC.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpPost]
+        public async Task<JsonResult> UploadFileAsync(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var fileName = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString() + Path.GetExtension(file.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\upload", fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+                return Json(new { success = true, filePath = @$"upload/{fileName}" });
+            }
+            return Json(new { success = false, filePath = string.Empty });
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(List<IFormFile> files)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/CEC.WebMVC/wwwroot/js/admin/products/add.js
@@ -0,0 +1,24 @@
+$('#formImageUpload').submit(function (e) {
+    e.preventDefault();
+    console.log(this.action);
+    $.ajax({
+        url: this.action,
+        type: this.method,
+        data: new FormData(this),
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            if (data?.success) {
+                $('#product-thumbnail-el').empty()
+                    .append('<img src="/' + data?.filePath + '" class="rounded img-thumbnail" alt="product image" loading="lazy" style="height:300px" >');
+                $('#ThumbnailUrl').val(data?.filePath);
+            } else {
+                $('#product-thumbnail-el').empty();
+            }
+        },
+        error: function (xhr, error, status) {
+            console.error(error, status);
+        }
+    })
+});
